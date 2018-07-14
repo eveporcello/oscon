79583af8c09996df487f7262cdb449a218954a7f
Eve Porcello <eve@moonhighway.com>
2018-07-14 13:05:29 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+var photos = [
+    { "id": "1", "name": "Dropping the Heart Chute", "description": "The heart chute is one of my favorite chutes", "githubLogin": "gPlake" },
+    { "id": "2", "name": "Enjoying the sunshine", "githubLogin": "sSchmidt" },
+    { "id": "3", "name": "Gunbarrel 25", "description": "25 laps on gunbarrel today", "githubLogin": "sSchmidt" }
+]
+
+var users = [
+    { "name": "Mike Hattrup", "githubLogin": "mHattrup" },
+    { "name": "Glen Plake", "githubLogin": "gPlake" },
+    { "name": "Scot Schmidt", "githubLogin": "sSchmidt" }
+]
