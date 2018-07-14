dfec37c3f609245c156499f1b8aa3da86af1c017
Eve Porcello <eve@moonhighway.com>
2018-07-14 13:25:40 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 var photos = [
-    { "id": "1", "name": "Dropping the Heart Chute", "description": "The heart chute is one of my favorite chutes", "githubLogin": "gPlake" },
-    { "id": "2", "name": "Enjoying the sunshine", "githubLogin": "sSchmidt" },
-    { "id": "3", "name": "Gunbarrel 25", "description": "25 laps on gunbarrel today", "githubLogin": "sSchmidt" }
+    { "id": "1", "name": "Dropping the Heart Chute", "description": "The heart chute is one of my favorite chutes", "githubLogin": "mParker" },
+    { "id": "2", "name": "Enjoying the sunshine", "githubLogin": "eSaugstad" },
+    { "id": "3", "name": "Gunbarrel 25", "description": "25 laps on gunbarrel today", "githubLogin": "mParker" }
 ]
 
 var users = [
-    { "name": "Mike Hattrup", "githubLogin": "mHattrup" },
-    { "name": "Glen Plake", "githubLogin": "gPlake" },
-    { "name": "Scot Schmidt", "githubLogin": "sSchmidt" }
+    { "name": "Elyse Saugstad", "githubLogin": "eSaugstad" },
+    { "name": "Michelle Parker", "githubLogin": "mParker" },
+    { "name": "Angel Collinson", "githubLogin": "aCollinson" }
 ]
